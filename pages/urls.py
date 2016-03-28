27ec6ae7602072f3e0f1e13ac4a01b32ad888d7a
Sergey Kanukov <s.kanukov@gmail.com>
2016-03-28 16:19:29 +0300

    Use namespace for pages app

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from pages import views
 
+
+app_name = 'pages'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
