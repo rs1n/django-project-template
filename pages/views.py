cc940e7edbe064e75d3787296f1032a117f25d34
Sergey Kanukov <s.kanukov@gmail.com>
2016-03-28 16:15:47 +0300

    Render templates instead of direct HttpResponse

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+
+# Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the pages index.")
+    return render(request, 'pages/index.html')
 
 
 def about(request):
-    return HttpResponse("Hello, world. You're at the pages about.")
+    return render(request, 'pages/about.html')
