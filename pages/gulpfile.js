2982668bdc1934661d39598f7f09bd91704d32ea
Sergey Kanukov <s.kanukov@gmail.com>
2016-03-30 17:16:20 +0300

    Add Gulp task for sass

--- pages/gulpfile.js
+++ pages/gulpfile.js
@@ -1,6 +1,37 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-  // place code for your default task here
-  console.log('Hello from Gulp.');
+var del = require('del');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+    // images: 'client/img/**/*',
+    // scripts: ['client/js/**/*.coffee', '!client/external/**/*.coffee'],
+    styles: 'assets/styles/**/*.scss'
+};
+
+// Not all tasks need to use streams
+// A gulpfile is just another node program and you can use any package available on npm
+gulp.task('clean', function() {
+    // You can use multiple globbing patterns as you would with `gulp.src`
+    return del(['static/pages']);
 });
+
+gulp.task('styles', function () {
+    return gulp.src(paths.styles)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('static/pages'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    // gulp.watch(paths.scripts, ['scripts']);
+    // gulp.watch(paths.images, ['images']);
+    gulp.watch(paths.styles, ['styles']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch', 'styles']);
