e28b6d3231ec548e52cec489f95bb8ad1d24d6ff
Sergey Kanukov <s.kanukov@gmail.com>
2016-03-30 18:09:54 +0300

    Add image processing. Move some dependencies to dev section

--- pages/gulpfile.js
+++ pages/gulpfile.js
@@ -3,11 +3,12 @@
 var del = require('del');
 var gulp = require('gulp');
 // var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
-    // images: 'client/img/**/*',
+    images: 'assets/images/**/*',
     // scripts: ['client/js/**/*.coffee', '!client/external/**/*.coffee'],
     styles: {
         all: 'assets/styles/**/*.scss',
@@ -22,20 +23,29 @@ gulp.task('clean', function() {
     return del(['static/pages']);
 });
 
+// Process SASS
 gulp.task('styles', ['clean'], function () {
     return gulp.src(paths.styles.main)
         .pipe(sourcemaps.init())
             .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('static/pages'));
+        .pipe(gulp.dest('static/pages/styles'));
+});
+
+// Copy all static images
+gulp.task('images', ['clean'], function() {
+  return gulp.src(paths.images)
+    // Pass in options to the task
+    .pipe(imagemin())
+    .pipe(gulp.dest('static/pages/images'));
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
     // gulp.watch(paths.scripts, ['scripts']);
-    // gulp.watch(paths.images, ['images']);
     gulp.watch(paths.styles.all, ['styles']);
+    gulp.watch(paths.images, ['images']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'styles']);
+gulp.task('default', ['watch', 'styles', 'images']);
