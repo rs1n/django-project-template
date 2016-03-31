f0b9661fdca10a679e9c290470527957f3cb6e81
Sergey Kanukov <s.kanukov@gmail.com>
2016-03-31 11:02:32 +0300

    Add scripts proccessing

--- pages/gulpfile.js
+++ pages/gulpfile.js
@@ -2,24 +2,32 @@
 
 var del = require('del');
 var gulp = require('gulp');
-// var concat = require('gulp-concat');
+var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+// var uglify = require('gulp-uglify');
 
 var paths = {
     images: 'assets/images/**/*',
-    // scripts: ['client/js/**/*.coffee', '!client/external/**/*.coffee'],
+    scripts: {
+        all: 'assets/scripts/**/*.js',
+        copy: [
+            'assets/scripts/index.js'
+        ],
+        join: [
+            'assets/scripts/vendor/jquery.js',
+            'assets/scripts/common.js'
+        ]
+    },
     styles: {
         all: 'assets/styles/**/*.scss',
         main: 'assets/styles/**/application.scss'
     }
 };
 
-// Not all tasks need to use streams
-// A gulpfile is just another node program and you can use any package available on npm
+// Clean dest folder
 gulp.task('clean', function() {
-    // You can use multiple globbing patterns as you would with `gulp.src`
     return del(['static/pages']);
 });
 
@@ -32,20 +40,42 @@ gulp.task('styles', ['clean'], function () {
         .pipe(gulp.dest('static/pages/styles'));
 });
 
+// Copy scripts
+gulp.task('scripts:copy', ['clean'], function() {
+    return gulp.src(paths.scripts.copy)
+        .pipe(sourcemaps.init())
+            // .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('static/pages/scripts'));
+});
+
+// Concat scripts
+gulp.task('scripts:join', ['clean'], function() {
+    return gulp.src(paths.scripts.join)
+        .pipe(sourcemaps.init())
+            // .pipe(uglify())
+            .pipe(concat('application.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('static/pages/scripts'));
+});
+
+// Process scripts
+gulp.task('scripts', ['scripts:copy', 'scripts:join']);
+
 // Copy all static images
 gulp.task('images', ['clean'], function() {
-  return gulp.src(paths.images)
-    // Pass in options to the task
-    .pipe(imagemin())
-    .pipe(gulp.dest('static/pages/images'));
+    return gulp.src(paths.images)
+        // Pass in options to the task
+        .pipe(imagemin())
+        .pipe(gulp.dest('static/pages/images'));
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-    // gulp.watch(paths.scripts, ['scripts']);
-    gulp.watch(paths.styles.all, ['styles']);
-    gulp.watch(paths.images, ['images']);
+    gulp.watch([
+        paths.scripts.all, paths.styles.all, paths.images
+    ], ['scripts', 'styles', 'images']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'styles', 'images']);
+gulp.task('default', ['watch', 'scripts', 'styles', 'images']);
