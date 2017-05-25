a5e8286b9f7f83e609093908e7bb0e456e90720d
Sergey Kanukov <s.kanukov@gmail.com>
2017-05-25 13:09:47 +0300

    Provide an example for setting the environment

--- project_name/settings/__init__.py
+++ project_name/settings/__init__.py
@@ -1,7 +1,8 @@
 """
 Default environment is "dev".
 
-To use another one set the "DJANGO_SETTINGS_MODULE" environment variable.
+To use another one set the "DJANGO_SETTINGS_MODULE" environment variable, e.g.:
+export DJANGO_SETTINGS_MODULE={{ project_name }}.settings.prod
 
 For more information see
 https://docs.djangoproject.com/en/{{ docs_version }}/topics/settings/#envvar-DJANGO_SETTINGS_MODULE
