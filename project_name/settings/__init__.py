0b42d8e7a4d43735548d03b2950b8ae52131f97a
Sergey Kanukov <s.kanukov@gmail.com>
2017-05-25 13:04:51 +0300

    Provide a link to docs for environment configuration

--- project_name/settings/__init__.py
+++ project_name/settings/__init__.py
@@ -1,3 +1,10 @@
-'Default environment.'
+"""
+Default environment is "dev".
+
+To use another one set the "DJANGO_SETTINGS_MODULE" environment variable.
+
+For more information see
+https://docs.djangoproject.com/en/{{ docs_version }}/topics/settings/#envvar-DJANGO_SETTINGS_MODULE
+"""
 
 from .dev import *
