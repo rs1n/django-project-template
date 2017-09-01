558add3d9c2c14bdd1c276a1cbc447c90db2c51b
skanukov <s.kanukov@gmail.com>
2017-09-01 13:36:25 +0300

    Include a sample request processing time logging middleware

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -1,3 +1,10 @@
 'Development environment settings'
 
 from .base import *
+
+# Custom middleware for development environment.
+DEV_MIDDLEWARE = [
+    'foo.middleware.LogProcessingTimeMiddleware',
+]
+
+MIDDLEWARE.extend(DEV_MIDDLEWARE)
